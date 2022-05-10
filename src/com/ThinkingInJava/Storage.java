8813750270d4c0125b5f68a1963348cae4dac00f
Denis Lazykin <dlazykin856@gmail.com>
2022-05-10 16:46:40 +0300

    Упражнение 2.6

---
+++ src/com/ThinkingInJava/Storage.java
@@ -0,0 +1,18 @@
+package com.ThinkingInJava;
+
+public class Storage {
+    /**
+     * Сколько байт потребуется для хранения данных
+     * определенной строки
+     * @param s строка, содержащая символы, которые нужно расчитать
+     * @return количество символов в строке
+     */
+    int storage(String s) {
+        return s.length() * 2;
+    }
+
+    public static void main(String[] args) {
+        Storage storage = new Storage();
+        System.out.println(storage.storage("9"));
+    }
+}

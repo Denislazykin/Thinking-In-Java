f033e78ff6bc54be0dd16e80d732a58059234442
Denis Lazykin <dlazykin856@gmail.com>
2022-05-10 16:27:08 +0300

    Упражнение 2.5

---
+++ src/com/ThinkingInJava/DataOnly.java
@@ -0,0 +1,14 @@
+package com.ThinkingInJava;
+
+public class DataOnly {
+    int i= 56;
+    double d = 21.21;
+    boolean b = true;
+
+    public static void main(String[] args) {
+        DataOnly dataOnly = new DataOnly();
+        System.out.println(dataOnly.i);
+        System.out.println(dataOnly.d);
+        System.out.println(dataOnly.b);
+    }
+}

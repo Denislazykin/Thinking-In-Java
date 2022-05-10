69060ff67153cb59a214032bb46bd408d4251501
Denis Lazykin <dlazykin856@gmail.com>
2022-05-10 15:58:52 +0300

    Первая программа. Добавил коменты

--- src/com/ThinkingInJava/HelloDate.java
+++ src/com/ThinkingInJava/HelloDate.java
@@ -2,7 +2,16 @@ package com.ThinkingInJava;
 
 import java.util.*;
 
+/**
+ * Первая программа - пример книги
+ * Выводит строку и текущее число
+ * @author Denis Lazykin
+ */
 public class HelloDate {
+    /**
+     * Точка входа в класс и приложение
+     * @param args Массив строковых аргументов
+     */
     public static void main(String[] args) {
         System.out.println("Hello, today: ");
         System.out.println(new Date());

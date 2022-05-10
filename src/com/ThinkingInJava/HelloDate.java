974d2e9e350a3d0019d51cabbb6cfbeb00b23b61
Denis Lazykin <dlazykin856@gmail.com>
2022-05-10 15:12:17 +0300

    Первая программа

---
+++ src/com/ThinkingInJava/HelloDate.java
@@ -0,0 +1,10 @@
+package com.ThinkingInJava;
+
+import java.util.*;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, today: ");
+        System.out.println(new Date());
+    }
+}

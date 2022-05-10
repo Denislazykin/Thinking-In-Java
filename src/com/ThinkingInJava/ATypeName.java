5fe4d51172d6a12bb565bb16099bf504b4f9d625
Denis Lazykin <dlazykin856@gmail.com>
2022-05-10 16:23:08 +0300

    Упражнение 2.3

---
+++ src/com/ThinkingInJava/ATypeName.java
@@ -0,0 +1,13 @@
+package com.ThinkingInJava;
+
+public class ATypeName {
+    String name;
+    char sex;
+    int age;
+    String country;
+
+    public static void main(String[] args) {
+        ATypeName aTypeName = new ATypeName();
+        System.out.println(aTypeName.country);
+    }
+}

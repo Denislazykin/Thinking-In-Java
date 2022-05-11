661163d60489964260967be8074411e4c8a47631
Denis Lazykin <dlazykin856@gmail.com>
2022-05-11 08:57:31 +0300

    Упражнение 2.10

--- src/com/ThinkingInJava/CommandLine.java
+++ src/com/ThinkingInJava/CommandLine.java
@@ -1,9 +1,13 @@
 package com.ThinkingInJava;
 
+/**
+ * Программа выводит три параметра командной строки
+ */
 public class CommandLine {
     public static void main(String[] args) {
         System.out.println("args[0] = " + args[0]);
         System.out.println("args[1] = " + args[1]);
         System.out.println("args[2] = " + args[2]);
+        System.out.println("args[2] = " + args[3]);
     }
 }

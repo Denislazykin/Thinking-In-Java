fee66f53856ad60405edbc6ede1cbfeee1d0d9e9
Denis Lazykin <dlazykin856@gmail.com>
2022-05-11 09:13:24 +0300

    Упражнение 2.13

---
+++ src/com/ThinkingInJava/Documentation2.java
@@ -0,0 +1,20 @@
+package com.ThinkingInJava;
+
+import java.util.*;
+
+// object/Documentation2.java
+
+/**
+ * <pre>
+ * Uses
+ * System.out.println(new Date());
+ * </pre>
+ */
+
+public class Documentation2 {
+    Date d = new Date();
+
+    void showDate() {
+        System.out.println("Date = " + d);
+    }
+}

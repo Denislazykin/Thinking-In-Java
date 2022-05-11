649c227065cffc22d7e42b7d142a9ea8a86ae06f
Denis Lazykin <dlazykin856@gmail.com>
2022-05-11 12:35:25 +0300

    Математические операторы

---
+++ src/com/ThinkingInJava/operators/MathOps.java
@@ -0,0 +1,52 @@
+package com.ThinkingInJava.operators;
+
+import java.util.*;
+
+public class MathOps {
+    public static void main(String[] args) {
+        // Создание и раскрутка генератора случайных чисел:
+        Random rand = new Random();
+        int i, j, k;
+        // Выбор значения от 1 до 100:
+        j = rand.nextInt(100) + 1;
+        System.out.println("j : " + j);
+        k = rand.nextInt(100) + 1;
+        System.out.println("k : " + k);
+        i = j + k;
+        System.out.println("j + k : " + i);
+        i = j - k;
+        System.out.println("j - k : " + i);
+        i = k / j;
+        System.out.println("k / j : " + i);
+        i = k * j;
+        System.out.println("k * j : " + i);
+        i = k % j;
+        System.out.println("k % j : " + i);
+        j %= k;
+        System.out.println("j %= k : " + j);
+        // Тесты для вещественных чисел:
+        float u, v, w; // так же можно использовать double
+        v = rand.nextFloat();
+        System.out.println("v : " + v);
+        w = rand.nextFloat();
+        System.out.println("w : " + w);
+        u = v + w;
+        System.out.println("v + w : " + u);
+        u = v - w;
+        System.out.println("v - w : " + u);
+        u = v * w;
+        System.out.println("v * w : " + u);
+        u = v / w;
+        System.out.println("v / w : " + u);
+        // Следующее также относится к типам char,
+        // byte, short, int, long, and double:
+        u += v;
+        System.out.println("u += v : " + u);
+        u -= v;
+        System.out.println("u -= v : " + u);
+        u *= v;
+        System.out.println("u *= v : " + u);
+        u /= v;
+        System.out.println("u /= v : " + u);
+    }
+}

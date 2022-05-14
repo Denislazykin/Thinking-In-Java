a0cbbba1ab1a31c727c74784374288906aa15e36
Denis Lazykin <dlazykin856@gmail.com>
2022-05-14 10:44:53 +0300

    Демонстрация применения инкремента и декремента

---
+++ src/com/ThinkingInJava/operators/AutoInc.java
@@ -0,0 +1,18 @@
+package com.ThinkingInJava.operators;
+
+/*
+Префиксный инкремент ++i
+Постфиксный инкремент i++
+ */
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i : " + i);
+        System.out.println("++i : " + ++i);
+        System.out.println("i++ : " + i++);
+        System.out.println("i : " + i);
+        System.out.println("--i : " + --i);
+        System.out.println("i-- : " + i--);
+        System.out.println("i : " + i);
+    }
+}

ea357a94bbe043053e8d94e1df480d1452e5262a
Denis Lazykin <dlazykin856@gmail.com>
2022-06-05 08:35:33 +0300

    Упражнение 4.2

---
+++ src/com/ThinkingInJava/controlStructures/Random25.java
@@ -0,0 +1,21 @@
+package com.ThinkingInJava.controlStructures;
+
+import java.util.Random;
+
+class Random25 {
+    public static void main(String[] args) {
+        Random rand1 = new Random();
+        Random rand2 = new Random();
+        for (int i = 0; i < 25; i++) {
+            int x = rand1.nextInt();
+            int y = rand2.nextInt();
+            if (x < y) {
+                System.out.println(x + " < " + y);
+            } else if (x > y) {
+                System.out.println(x + " > " + y);
+            } else {
+                System.out.println(x + " = " + y);
+            }
+        }
+    }
+}

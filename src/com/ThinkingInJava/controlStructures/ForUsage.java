b9fb8ef7b0d7c39161366c9f3864b19ec2bfd28a
Denis Lazykin <dlazykin856@gmail.com>
2022-06-05 08:08:15 +0300

    Упражнение 4.1

---
+++ src/com/ThinkingInJava/controlStructures/ForUsage.java
@@ -0,0 +1,11 @@
+package com.ThinkingInJava.controlStructures;
+/*
+Программа выводит числа от 1 до 100
+ */
+public class ForUsage {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}

efce014362c5a829358e53deecba49acf99c39ef
Denis Lazykin <dlazykin856@gmail.com>
2022-06-06 14:37:41 +0300

    Упражнение 5.2

---
+++ src/com/ThinkingInJava/initializationAndCompletion/StringInit2.java
@@ -0,0 +1,21 @@
+package com.ThinkingInJava.initializationAndCompletion;
+/*
+Инициализациия в точке определения
+и в конструкторе.
+ */
+public class StringInit2 {
+    String string;
+    String string1 = "hello";
+    String string2;
+
+    public StringInit2() {
+        string2 = "good bye";
+    }
+
+    public static void main(String[] args) {
+        StringInit2 stringInit2 = new StringInit2();
+        System.out.println("stringInit2.string: " + stringInit2.string);
+        System.out.println("stringInit2.string1: " + stringInit2.string1);
+        System.out.println("stringInit2.string2: " + stringInit2.string2);
+    }
+}

70e24a7c19faeabde91b53804976fdd21b896cac
Denis Lazykin <dlazykin856@gmail.com>
2022-06-06 14:24:30 +0300

    Упражнение 5.1

---
+++ src/com/ThinkingInJava/initializationAndCompletion/StringInit.java
@@ -0,0 +1,10 @@
+package com.ThinkingInJava.initializationAndCompletion;
+
+public class StringInit {
+    String string;
+
+    public static void main(String[] args) {
+        StringInit stringInit = new StringInit();
+        System.out.println(stringInit.string);
+    }
+}

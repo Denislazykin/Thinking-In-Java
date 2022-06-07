810c33f6281cfc264c4e9bf6d99716727b8f42d3
Denis Lazykin <dlazykin856@gmail.com>
2022-06-07 13:27:25 +0300

    Упражнение 5.3 update

--- src/com/ThinkingInJava/initializationAndCompletion/Book.java
+++ src/com/ThinkingInJava/initializationAndCompletion/Book.java
@@ -5,7 +5,12 @@ public class Book {
         System.out.println("Object is created");
     }
 
+    public Book(String s) {
+        System.out.println("Object is created " + s);
+    }
+
     public static void main(String[] args) {
         Book book = new Book();
+        Book book1 = new Book("GodGather");
     }
 }

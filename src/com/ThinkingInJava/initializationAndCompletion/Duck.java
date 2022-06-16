c1ffe062b2422f78207d0370bbb1ffcd85aa7c14
Denis Lazykin <dlazykin856@gmail.com>
2022-06-16 07:13:30 +0300

    Упражнение 5.10 + 5.11

--- src/com/ThinkingInJava/initializationAndCompletion/Duck.java
+++ src/com/ThinkingInJava/initializationAndCompletion/Duck.java
@@ -5,17 +5,21 @@ package com.ThinkingInJava.initializationAndCompletion;
  */
 class Duck {
     boolean loggedIn = false;
+
     Duck(boolean logStatus) {
         loggedIn = logStatus;
     }
+
     void logIn() {
         loggedIn = true;
     }
+
     void logOut() {
         loggedIn = false;
     }
+
     protected void finalize() {
-        if(loggedIn)
+        if (loggedIn)
             System.out.println("Error: still logged in");
         // Normally, you'll also do this:
         // super.finalize(); // Call the base-class version
@@ -26,12 +30,19 @@ class TerminalCondition {
     public static void main(String[] args) {
         Duck duck = new Duck(true);
         Duck duck1 = new Duck(true);
-        // Proper cleanup: log out of bank1 before going home
+        // Proper cleanup: log out of duck before going home
         duck.logOut();
-        // Drop the reference, forget to cleanup:
-        new Duck(true);
-        // Force garbage collection and finalization:
+        // Forget to logout of duck1 and unnamed new duck
+        // Attempts to finalize any missed ducks:
+        System.out.println("Try 1: ");
+        System.runFinalization();
+        System.out.println("Try 2: ");
+        Runtime.getRuntime().runFinalization();
+        System.out.println("Try 3: ");
         System.gc();
+        System.out.println("Try 4: ");
+        // using deprecated since 1.1 method:
+        System.runFinalization();
     }
 }
 

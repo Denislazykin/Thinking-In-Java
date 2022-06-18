750e366e1b551a05500f043be6c770df717394ca
Денис <dzaka89@gmail.com>
2022-06-18 13:37:38 +0300

    Упражнение 5.22

--- src/com/ThinkingInJava/initializationAndCompletion/Moneys.java
+++ src/com/ThinkingInJava/initializationAndCompletion/Moneys.java
@@ -11,3 +11,35 @@ class MoneysEnumOrder {
         }
     }
 }
+
+class Wallet {
+    Moneys moneys;
+
+    public static void main(String[] args) {
+        for (Moneys moneys : Moneys.values()) {
+            System.out.print("Nominal value: ");
+            switch (moneys) {
+                case ONE:
+                    System.out.println("$1");
+                    break;
+                case FIVE:
+                    System.out.println("$5");
+                    break;
+                case TEN:
+                    System.out.println("$10");
+                    break;
+                case TWENTY:
+                    System.out.println("$20");
+                    break;
+                case FIFTY:
+                    System.out.println("$50");
+                    break;
+                case HUNDRED:
+                    System.out.println("$100");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

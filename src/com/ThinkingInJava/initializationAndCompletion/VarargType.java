2de554168898ff9a5fe41d957736690b4217cd25
Denis Lazykin <dlazykin856@gmail.com>
2022-06-18 10:51:00 +0300

    Список аргументов переменной среды превращается в массив, а при отсутствии элементов он становится массивом 0 длинны

---
+++ src/com/ThinkingInJava/initializationAndCompletion/VarargType.java
@@ -0,0 +1,25 @@
+package com.ThinkingInJava.initializationAndCompletion;
+
+/*
+Список аргументов переменной среды превращается в массив,
+а при отсутствии элементов он становится массивом 0 длинны
+ */
+public class VarargType {
+    static void f(Character... args) {
+        System.out.print(args.getClass());
+        System.out.println(" length " + args.length);
+    }
+
+    static void g(int... args) {
+        System.out.print(args.getClass());
+        System.out.println(" length " + args.length);
+    }
+
+    public static void main(String[] args) {
+        f('a');
+        f();
+        g(1);
+        g();
+        System.out.println("int[]: " + new int[0].getClass());
+    }
+}

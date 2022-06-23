f1124495bdaeb6dbe24da4d4afd20ede96914d5d
Денис <dzaka89@gmail.com>
2022-06-23 15:52:31 +0300

    Упражнение 6.1

---
+++ src/com/ThinkingInJava/access/Collision.java
@@ -0,0 +1,12 @@
+package com.ThinkingInJava.access;
+
+public class Collision {
+    public void main(String[] args) {
+        // Vector v = new Vector(); // ambiquous collision
+        com.ThinkingInJava.access.Collision.Vector v1 = new com.ThinkingInJava.access.Collision.Vector();
+        java.util.Vector v2 = new java.util.Vector();
+    }
+
+    public class Vector {
+    }
+}

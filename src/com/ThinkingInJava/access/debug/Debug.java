1fe0d598565d53b6a2ec265b0386d997f554143b
Denis Lazykin <dlazykin856@gmail.com>
2022-06-23 17:15:49 +0300

    Упражнение 6.3 исправления

--- src/com/ThinkingInJava/access/debug/Debug.java
+++ src/com/ThinkingInJava/access/debug/Debug.java
@@ -3,4 +3,8 @@ package com.ThinkingInJava.access.debug;
 public class Debug {
     public static void debug(String s) {
     }
+
+    public static void main(String[] args) {
+        debug("sd");
+    }
 }
--- src/com/ThinkingInJava/access/debugoff/Debug.java
+++ src/com/ThinkingInJava/access/debugoff/Debug.java
@@ -4,4 +4,8 @@ public class Debug {
     public static void debug(String s) {
         System.out.println(s);
     }
+
+    public static void main(String[] args) {
+        debug("s");
+    }
 }

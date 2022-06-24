665052bd02f4c306231752e7c4770741e18a6aad
Denis Lazykin <dlazykin856@gmail.com>
2022-06-24 08:44:07 +0300

    Упражнение 6.9.Ошибка компилятора, потому что: Упакованный класс не является общедоступным

---
+++ src/com/ThinkingInJava/access/Foreign.java
@@ -0,0 +1,9 @@
+package com.ThinkingInJava.access;
+
+import com.ThinkingInJava.access.local.*;
+
+public class Foreign {
+    public static void main(String[] args) {
+        PackedClass packedClass = new PackedClass();
+    }
+}
---
+++ src/com/ThinkingInJava/access/local/PackedClass.java
@@ -0,0 +1,7 @@
+package com.ThinkingInJava.access.local;
+
+public class PackedClass {
+    public PackedClass() {
+        System.out.println("Create class in the package");
+    }
+}

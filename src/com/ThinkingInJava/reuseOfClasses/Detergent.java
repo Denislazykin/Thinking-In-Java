0f0a7cb80f4018bb3c350f7dee9fe69bc4454ffc
Denis Lazykin <dlazykin856@gmail.com>
2022-06-25 12:10:55 +0300

    Синтаксис наследования и его свойства

---
+++ src/com/ThinkingInJava/reuseOfClasses/Detergent.java
@@ -0,0 +1,62 @@
+package com.ThinkingInJava.reuseOfClasses;
+
+/*
+Синтаксис наследования и его свойства
+ */
+class Cleanser {
+    private String s = "Cleanser";
+
+    public void append(String a) {
+        s += a;
+    }
+
+    public void dilute() {
+        append(" dilute()");
+    }
+
+    public void apply() {
+        append(" apply()");
+    }
+
+    public void scrub() {
+        append(" scrub()");
+    }
+
+    public String toString() {
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Cleanser c = new Cleanser();
+        c.dilute();
+        c.apply();
+        c.scrub();
+        System.out.println(c);
+    }
+}
+
+public class Detergent extends Cleanser {
+    //Изменяем метод
+    public void scrub() {
+        append(" Detergent.scrub()");
+        //вызываем метод базового класс
+        super.scrub();
+    }
+
+    //Добавляем новые методы к интерфейсу
+    public void foam() {
+        append(" foam()");
+    }
+
+    //Проверяем новый класс
+    public static void main(String[] args) {
+        Detergent d = new Detergent();
+        d.dilute();
+        d.apply();
+        d.scrub();
+        d.foam();
+        System.out.println(d);
+        System.out.println("Checking the base class");
+        Cleanser.main(args);
+    }
+}

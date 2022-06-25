2f4939427fae695fe1c841a1b06921c039469f7d
Denis Lazykin <dlazykin856@gmail.com>
2022-06-25 12:17:32 +0300

    Упражнение 7.2

--- src/com/ThinkingInJava/reuseOfClasses/Detergent.java
+++ src/com/ThinkingInJava/reuseOfClasses/Detergent.java
@@ -60,3 +60,25 @@ public class Detergent extends Cleanser {
         Cleanser.main(args);
     }
 }
+
+class Sterilizer extends Detergent{
+    public void scrub() {
+        append(" Sterilizer.scrub()");
+        super.scrub();
+    }
+    public void sterilize() {
+        append(" sterilize()");
+    }
+
+    public static void main(String[] args) {
+        Sterilizer s = new Sterilizer();
+        s.dilute();
+        s.apply();
+        s.scrub();
+        s.foam();
+        s.sterilize();
+        System.out.println(s);
+        System.out.println("Testing base class:");
+        Detergent.main(args);
+    }
+}

cef38920c3b9679742f5519550612818308112ea
Денис <dzaka89@gmail.com>
2022-06-26 07:42:06 +0300

    Упражнение 7.7

--- src/com/ThinkingInJava/reuseOfClasses/Cc.java
+++ src/com/ThinkingInJava/reuseOfClasses/Cc.java
@@ -1,21 +1,27 @@
 package com.ThinkingInJava.reuseOfClasses;
 
 class Aa {
-    Aa() {
+    Aa(String name, int age) {
         System.out.println("A's constructor");
     }
 }
 
-class Bb {
-    Bb() {
+class Bb extends Aa {
+    Bb(String name, int age) {
+        super(name, age);
         System.out.println("b's constructor");
     }
 }
 
 class Cc extends Aa {
-    Bb bb = new Bb();
+    Cc(String name, char sex) {
+        super("Den", 32);
+        System.out.println("Cc's constructor");
+    }
+
+    Bb bb = new Bb("Nasy", 33);
 
     public static void main(String[] args) {
-        Cc cc = new Cc();
+        Cc cc = new Cc("Ivan", 'M');
     }
 }

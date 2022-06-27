d39f9d9e7ede37f349ab1192c43699cf74238c25
Денис <dzaka89@gmail.com>
2022-06-27 06:41:09 +0300

    Упражнение 7.12

--- src/com/ThinkingInJava/reuseOfClasses/Root.java
+++ src/com/ThinkingInJava/reuseOfClasses/Root.java
@@ -1,51 +1,81 @@
 package com.ThinkingInJava.reuseOfClasses;
 
 public class Root {
-    Component1 component1Root = new Component1(164);
-    Component2 component2Root = new Component2(52);
-    Component3 component3Root = new Component3(33);
+    Component1 component1Root;
+    Component2 component2Root;
+    Component3 component3Root;
 
-    public Root(String name, int age) {
-        component1Root = new Component1(0);
-        component2Root = new Component2(0);
-        component3Root = new Component3(0);
+    public Root() {
         System.out.println("Root constructor");
+        component1Root = new Component1();
+        component2Root = new Component2();
+        component3Root = new Component3();
+    }
+
+    void dispose() {
+        component3Root.dispose();
+        component2Root.dispose();
+        component1Root.dispose();
+        System.out.println("Root.dispose()");
     }
 }
 
 class Component1 {
-    public Component1(int height) {
+    public Component1() {
         System.out.println("Component1 constructor");
+    }
+
+    void dispose() {
+        System.out.println("Component1.dispose()");
     }
 }
 
 class Component2 {
-    public Component2(int weight) {
+    public Component2() {
         System.out.println("Component2 constructor");
+    }
+
+    void dispose() {
+        System.out.println("Component2.dispose()");
     }
 }
 
 class Component3 {
-    public Component3(int volume) {
+    public Component3() {
         System.out.println("Component3 constructor");
+    }
+
+    void dispose() {
+        System.out.println("Component3.dispose()");
     }
 }
 
 class Stem extends Root {
-    Component1 component1Stem = new Component1(195);
-    Component2 component2Stem = new Component2(77);
-    Component3 component3Stem = new Component3(60);
+    Component1 component1Stem;
+    Component2 component2Stem;
+    Component3 component3Stem;
 
-    Stem(String country) {
-        super("", 0);
-        component1Stem = new Component1(1);
-        component2Stem = new Component2(1);
-        component3Stem = new Component3(1);
+    Stem() {
+        super();
         System.out.println("Stem constructor");
+        component1Stem = new Component1();
+        component2Stem = new Component2();
+        component3Stem = new Component3();
+    }
+
+    void dispose(){
+        component3Stem.dispose();
+        component2Stem.dispose();
+        component1Stem.dispose();
+        System.out.println("Stem.dispose()");
     }
 
     public static void main(String[] args) {
-        Stem stem = new Stem("RF");
-        Root root = new Root("Den", 32);
+        Stem stem = new Stem();
+        try {
+            // Code and exception handling...
+        } finally {
+            stem.dispose();
+        }
     }
 }

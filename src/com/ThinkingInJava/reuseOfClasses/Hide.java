229dbcc755220cf2f399eb00565fa1289264f4ec
Денис <dzaka89@gmail.com>
2022-06-27 07:01:50 +0300

    Перегрузка

--- src/com/ThinkingInJava/reuseOfClasses/Hide.java
+++ src/com/ThinkingInJava/reuseOfClasses/Hide.java
@@ -1,30 +1,44 @@
 package com.ThinkingInJava.reuseOfClasses;
+
 /*
 Перегрузка имени метода их базового класса
 в производном классе, не скроет базовую версию метода
  */
-class Homer{
-    char doh(char c){
+class Homer {
+    char doh(char c) {
         System.out.println("doh(char)");
         return 'd';
     }
-    float doh(float f){
+
+    float doh(float f) {
         System.out.println("doh(float)");
         return 3.14f;
     }
 }
-class Milhouse{}
-class Bart extends Homer{
-    void doh(Milhouse milhouse){
+
+class Milhouse {
+}
+
+class Bart extends Homer {
+    void doh(Milhouse milhouse) {
         System.out.println("doh(Milhouse)");
     }
 }
-public class Hide {
-    public static void main(String[] args) {
-        Bart bart = new Bart();
-        bart.doh(1);
-        bart.doh('g');
-        bart.doh(4.33f);
-        bart.doh(new Milhouse());
+
+class Lisa extends Homer {
+    /*  @Override
+      void doh(Milhouse milhouse) {
+          System.out.println("doh(Milhouse)");
+      }
+  }
+     */
+    public class Hide {
+        public static void main(String[] args) {
+            Bart bart = new Bart();
+            bart.doh(1);
+            bart.doh('g');
+            bart.doh(4.33f);
+            bart.doh(new Milhouse());
+        }
     }
 }

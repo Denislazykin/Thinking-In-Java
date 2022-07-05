ba49e037bedf8b0792d288d3974b1f9b6ccf42d6
Denis Lazykin <dlazykin856@gmail.com>
2022-07-05 09:42:48 +0300

    Наследование и восходящее преобразование

---
+++ src/com/ThinkingInJava/Wind.java
@@ -0,0 +1,18 @@
+package com.ThinkingInJava;
+
+class Instrument {
+    public void play(){};
+    static void tune(Instrument i){
+        i.play();
+    }
+}
+/*
+Объекты Wind так же явлдяются объектами Instrument
+поскольку они имеют тот же интерфейс
+ */
+public class Wind extends Instrument{
+    public static void main(String[] args) {
+        Wind flute = new Wind();
+        Instrument.tune(flute);
+    }
+}

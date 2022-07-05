7500766cb56697feaa3d68a2656df9e915037c4f
Denis Lazykin <dlazykin856@gmail.com>
2022-07-05 07:31:40 +0300

    Ключевое слово protected

---
+++ src/com/ThinkingInJava/reuseOfClasses/orc/Villain.java
@@ -0,0 +1,20 @@
+package com.ThinkingInJava.reuseOfClasses.orc;
+
+public class Villain {
+    private String name;
+
+    protected void set(String nm) {
+        name = nm;
+    }
+
+    public Villain(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Villain{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}

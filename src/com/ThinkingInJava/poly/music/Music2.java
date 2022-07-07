d4631435ed094eb28e8c0a7f73632d8ab9cebb1e
Denis Lazykin <dlazykin856@gmail.com>
2022-07-07 09:38:21 +0300

    Перегрузка вместо восходящего преобразования

---
+++ src/com/ThinkingInJava/poly/music/Music2.java
@@ -0,0 +1,38 @@
+package com.ThinkingInJava.poly.music;
+
+class Stringed extends Instrument {
+    @Override
+    public void play(Note note) {
+        System.out.println("Stringed.play() " + note);
+    }
+}
+
+class Brass extends Instrument {
+    @Override
+    public void play(Note note) {
+        System.out.println("Brass.play() " + note);
+    }
+}
+
+public class Music2 {
+    public static void tune (Wind i){
+        i.play(Note.MIDDLE_C);
+    }
+
+    public static void tune (Stringed i){
+        i.play(Note.C_SHARP);
+    }
+
+    public static void tune (Brass i){
+        i.play(Note.B_FLAT);
+    }
+
+    public static void main(String[] args) {
+        Wind flute = new Wind();
+        Stringed violin = new Stringed();
+        Brass frenchHorn = new Brass();
+        tune(flute); //без восходящего преобразования
+        tune(violin);
+        tune(frenchHorn);
+    }
+}
--- src/com/ThinkingInJava/poly/music/Wind.java
+++ src/com/ThinkingInJava/poly/music/Wind.java
@@ -3,6 +3,6 @@ package com.ThinkingInJava.poly.music;
 public class Wind extends Instrument {
     @Override
     public void play(Note note) {
-        System.out.println("Wind.play()" + note);
+        System.out.println("Wind.play() " + note);
     }
 }

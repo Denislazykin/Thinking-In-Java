eb2bf574b794dfdd63cefa2153044d62137e05f4
Denis Lazykin <dlazykin856@gmail.com>
2022-07-11 10:22:19 +0300

    Наследование и завершающее действие.edit file Description

--- src/com/ThinkingInJava/poly/frog/Description.java
+++ src/com/ThinkingInJava/poly/frog/Description.java
@@ -5,7 +5,7 @@ public class Description {
 
     public Description(String s) {
         this.s = s;
-        System.out.println("Create Description ");
+        System.out.println("Create Description " + s);
     }
 
     protected void dispose() {
